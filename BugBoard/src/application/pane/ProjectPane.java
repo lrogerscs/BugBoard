021a97871527e71126b909d4beacc05538836810
Lee Rogers <lrogers0941@gmail.com>
2023-11-14 20:02:29 -0800

    Added a delete button to projects, increased abstraction.

--- BugBoard/src/application/pane/ProjectPane.java
+++ BugBoard/src/application/pane/ProjectPane.java
@@ -2,8 +2,8 @@ package application.pane;
 
 import application.controller.EditProjectController;
 import application.project.Project;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
-import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -19,7 +19,7 @@ import javafx.stage.Stage;
 public class ProjectPane extends HBox {
    Project project;
    Label name, date;
-   Button edit;
+   Button edit, delete;
    HBox buttonPane;
    
    public ProjectPane(Project project) {
@@ -27,34 +27,38 @@ public class ProjectPane extends HBox {
       name = new Label(this.project.getName());
       date = new Label(this.project.getDate().toString());
       edit = new Button("Edit");
+      delete = new Button("Delete");
       buttonPane = new HBox();
       
-      buttonPane.setAlignment(Pos.CENTER_RIGHT);
+      buttonPane.setStyle("-fx-alignment: center-right; -fx-spacing: 10");
       setHgrow(buttonPane, Priority.ALWAYS);
       
       // Set button behavior.
-      edit.setOnAction(event -> {
-         try {
-            FXMLLoader loader = new FXMLLoader(getClass().getClassLoader().getResource("fxml/edit_project.fxml"));
-            Parent root = loader.load();
-            EditProjectController controller = loader.getController();
-            Scene scene = new Scene(root);
-            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-            
-            stage.setScene(scene);
-            stage.show();
-            controller.setProject(this.project);
-         } catch (Exception e) {
-            e.printStackTrace();
-         }
-      });
+      edit.setOnAction(event -> loadEditProject(event));
       
-      buttonPane.getChildren().add(edit);
+      buttonPane.getChildren().addAll(edit, delete);
       getChildren().addAll(this.name, this.date, buttonPane);
       
       this.name.getStyleClass().add("project-pane-label");
       this.date.getStyleClass().add("project-pane-label");
       edit.getStyleClass().add("edit-button");
+      delete.getStyleClass().add("delete-button");
       getStyleClass().add("info-pane");
+   }
+   
+   private void loadEditProject(ActionEvent event) {
+      try {
+         FXMLLoader loader = new FXMLLoader(getClass().getClassLoader().getResource("fxml/edit_project.fxml"));
+         Parent root = loader.load();
+         EditProjectController controller = loader.getController();
+         Scene scene = new Scene(root);
+         Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+         
+         stage.setScene(scene);
+         stage.show();
+         controller.setProject(this.project);
+      } catch (Exception e) {
+         e.printStackTrace();
+      }
    }
 }
